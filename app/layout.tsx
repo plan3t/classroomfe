baa5732091431781be1365ff3e044fcad73fce10
Plan3t <plan3t@live.de>
2026-05-20 16:35:06 +0200

    Fasse gleiche Warenkorb-Artikel zu einer Position zusammen

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
-  title: 'LinguaClass',
-  description: 'Begleitapp für Brettspiele mit Spielmodus, Lebensmitteln und Einkaufsdaten',
+  title: 'Smart Eat',
+  description: 'Smart Eat ist die Brettspiel-Begleitapp für Spielmodus, Lebensmittel und Einkaufsdaten.',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
